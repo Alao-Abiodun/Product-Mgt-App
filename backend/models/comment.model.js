476300556bf9cf923fc5719e09f808b2cfd9e409
Alao-Abiodun <46402042+Alao-Abiodun@users.noreply.github.com>
2022-07-19 11:32:07 +0100

    chore:create a model for users product and comment

---
+++ backend/models/comment.model.js
@@ -0,0 +1,18 @@
+const {Schema, model} = require('mongoose');
+
+const commentSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    comments: {
+        type: String,
+    }
+}, {
+    timestamps: true
+})
+
+const Comment = model('Comment', commentSchema);
+
+module.exports = Comment;
---
+++ backend/models/product.model.js
@@ -0,0 +1,46 @@
+const {Schema, model} = require('mongoose');
+const validator = require('validator');
+
+const geoSchema = new Schema({
+    type: {
+        type: String,
+        enum: ['Point'],
+        default: 'Point'
+    },
+    coordinates: {
+        type: [Number],
+        index: '2dsphere'
+    }
+})
+
+const productSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    }
+    comment: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Comment'
+    }]
+    name: {
+        type: String,
+        require: true
+    },
+    image: {
+        type: String,
+        required: true,
+        default:
+            "https://res.cloudinary.com/oluwatobiloba/image/upload/v1628753027/Grazac/avatar_cihz37.png", 
+    },
+    geo_details: geoSchema,
+
+}, {
+    timestamps: true
+})
+
+productSchema.index({coordinates: '2dsphere'})
+
+const Product = require('Product', productSchema);
+
+module.exports = Product;
